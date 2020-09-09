f4d2b72bc64eaed96ac89cf0f157c5e7dde689a7
bhuvanesh09 <bhuvanesh.sridharan@research.iiit.ac.in>
2020-09-09 11:16:40 +0530

    Made better handling of cd to home, etc

--- A2/shell/commands/cd.c
+++ A2/shell/commands/cd.c
@@ -2,13 +2,17 @@
 // Created by simpl on 03-09-2020.
 //
 
+#include <stdlib.h>
 #include "cd.h"
 
 int cd(struct shellState *currentState, char **commandArray, int numParts){
     if(strcmp(commandArray[1]," ")==0 || strcmp(commandArray[1], "")==0 || strcmp(commandArray[1], ".")==0) {
         return 1;
-    }else if(strcmp(commandArray[1],"~") == 0){
-        int suc = chdir(currentState->homePath);
+    }else if(commandArray[1][0] == '~'){
+        char *dir = (char *) malloc(sizeof(char)*1000);
+        strcpy(dir, currentState->homePath);
+        strcat(dir, commandArray[1]+1);
+        int suc = chdir(dir);
         if(suc<0){
             perror(currentState->homePath);
             return -1;
