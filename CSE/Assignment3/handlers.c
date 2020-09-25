f77ca43a14d74de97eb2b50ef42cf8ce330e7752
bhuvanesh09 <bhuvanesh.sridharan@research.iiit.ac.in>
2020-09-25 14:15:37 +0530

    Cloned A2 for A3

--- CSE/Assignment3/handlers.c
+++
@@ -1,5 +0,0 @@
-#include"header.h"
-
-void handler1(int sig_num){}
-
-void handler2(int sig_num){}
--- CSE/Assignment4/Q1/a.py
+++
@@ -1,6 +0,0 @@
-from random import randint
-N = 1000
-print(N)
-for i in range(N):
-	print(randint(1,1000000000),end=" ")
-print()
