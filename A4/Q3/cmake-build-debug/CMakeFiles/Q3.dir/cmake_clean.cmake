1a77262bed8eaa6af6ea16b59dea9fa97d11efe8
bhuvanesh09 <bhuvanesh.sridharan@research.iiit.ac.in>
2020-10-17 00:19:40 +0530

    Starting Q3

---
+++ A4/Q3/cmake-build-debug/CMakeFiles/Q3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Q3.dir/main.c.o"
+  "Q3.pdb"
+  "Q3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Q3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ A4/Q3/main.h
@@ -0,0 +1,8 @@
+//
+// Created by simpl on 17-10-2020.
+//
+
+#ifndef Q3_MAIN_H
+#define Q3_MAIN_H
+
+#endif //Q3_MAIN_H
